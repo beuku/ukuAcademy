07d733bfc775a5698d19f5f64c14c17b22d277dd
rsanmarting <104087552+rsanmarting@users.noreply.github.com>
2024-11-27 13:11:48 -0300

    orden-carpetas

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Footer from './components/Footer';
-import { ThemeProvider } from '@mui/material/styles';
-import darkTheme from './Theme' 
+
 import './styles/App.css'
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-        <Header />
-        <Formulario />
-        <Footer />
-    </ThemeProvider>
+    <div>
+      <Header />
+      <Formulario />
+      <Footer />
+    </div>
   );
 };
 
