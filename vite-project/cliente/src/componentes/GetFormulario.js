cbdce269bea0e98628a6f9356d517f595f5d1fa9
Thvich <vherrera2018@udec.cl>
2024-12-12 14:46:58 -0300

    id

--- vite-project/cliente/src/componentes/GetFormulario.js
+++ vite-project/cliente/src/componentes/GetFormulario.js
@@ -7,11 +7,11 @@ import { Button } from '@mui/material'
 function GetFormulario() {
   const [data, setData] = useState([]);
 
-  const handleDelete = async (e) =>{
+  const handleDelete = async (e, id) =>{
     e.preventDefault();
     try{
-      console.log(`http://localhost:4000/formularios/${e._id}`)
-    const response = await fetch(`http://localhost:4000/formularios/${e._id}`,{
+      console.log(`http://localhost:4000/formularios/${id}`)
+    const response = await fetch(`http://localhost:4000/formularios/${id}`,{
       method: 'delete',
       
     });
@@ -57,7 +57,7 @@ function GetFormulario() {
 
           <div className="conter">  
             <div className="boton2">
-             <Button variant="contained" color="error" onClick={handleDelete}>Eliminar</Button>
+             <Button variant="contained" color="error"  onClick={handleDelete(this, item._id)} >Eliminar</Button>
             </div>
 
             <div className="boton2">
