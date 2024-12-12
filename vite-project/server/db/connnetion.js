67c485ef913304e757999581367057eea8af52c6
Thvich <vherrera2018@udec.cl>
2024-12-12 15:12:18 -0300

    yep

--- vite-project/server/db/connnetion.js
+++ vite-project/server/db/connnetion.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://vherrera2018:frGg7pGaXQfIC1rb@sinami.js6qqcc.mongodb.net/?retryWrites=true&w=majority&appName=Sinami')
+mongoose.connect('mongodb://localhost:27017')
